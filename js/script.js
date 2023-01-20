fcfd3893c481f74e37c124aee51026973141138a
Mario Arthur <marioarthursm@outlook.com>
2023-01-20 15:22:01 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector(' header button')
 
-const data = { 
-    run: ['01-01', '01-02', '01-06'], 
-    water: ['01-04', '01-05'],
-    food: ['01-01', '01-03'],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-BR').slice(0, -5)
+    const dayExists= nlwSetup.dayExists(today)
+
+    if (dayExists) {
+        return alert('Você já adicionou um hábito hoje!')
+    }
+    return alert('Hábito adicionado com sucesso!'),
+    nlwSetup.addDay(today)
+}
+
+function save() {
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
   }
   
+  const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
   nlwSetup.setData(data)
   nlwSetup.load()
